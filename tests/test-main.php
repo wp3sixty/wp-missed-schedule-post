0ed14bde3f39d0ddb09f47c6d7dff77fbf4df74a
Faishal Saiyed <saiyedfaishal@gmail.com>
2019-10-14 14:56:52 -0400

    Remove unwanted debug code and fix transient issue

--- tests/test-main.php
+++ tests/test-main.php
@@ -22,11 +22,12 @@ class MainTest extends WP_UnitTestCase {
 		$newtimestamp = date('Y-m-d H:i:s',  strtotime(current_time( 'mysql', 0 ) . ' + 1 minutes') );
 		$p = $this->factory->post->create( array( 'post_title' => 'Test Post', 'post_date' => $newtimestamp, 'post_status' => 'future' ) );
 		$post= get_post($p);
-		var_dump(current_time( 'mysql', 0 ));
-		var_dump($post->post_date);
 		$this->assertEquals( 'future',$post->post_status );
 		sleep(60);
+
+		delete_transient( 'wp_scheduled_missed_time' );
 		nv_wpmsp_init();
+
 		$post= get_post($p);
 		$this->assertEquals( $post->post_status, 'publish' );
 	}
