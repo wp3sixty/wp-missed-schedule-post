5e768fa8611a0c16359ecb7227ba8baf352c1555
Faishal Saiyed <saiyedfaishal@gmail.com>
2019-10-14 13:36:09 -0400

    Added phpcs, phpunit for base plugins

--- wp-missed-schedule-posts.php
+++ wp-missed-schedule-posts.php
@@ -1,33 +1,38 @@
 <?php
-/*
-Plugin Name: WP Missed Schedule Posts
-Description: Auto publish future post missed by WordPress cron
-Author: newvariable
-Contributors: newvariable,wp3sixty,sanketparmar,divyarajmasani,shaileesheth
-Author URI: https://newvariable.com
-Version: 1.1
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
- */
+/**
+ * Plugin Name: WP Missed Schedule Posts
+ * Description: Auto publish future post missed by WordPress cron
+ * Author: newvariable
+ * Contributors: newvariable,wp3sixty,sanketparmar,divyarajmasani,shaileesheth
+ * Author URI: https://newvariable.com
+ * Version: 1.1
+ * License: GPLv2 or later
+ * Text Domain: nv-wpmsp
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package Wp_Missed_Schedule_Posts
+ **/
 
-//bail of not wordpress path
+// bail of not WordPress path.
 if ( false === defined( 'ABSPATH' ) ) {
 	return;
 }
-//plugin basename for further refrence
+// plugin basename for further refrence.
 $nv_wpms_base_name = plugin_basename( __FILE__ );
 
-//Set default check interval - every 10 min
+// Set default check interval - every 10 min.
 if ( false === defined( 'WPMSP_INTERVAL' ) ) {
 	define( 'WPMSP_INTERVAL', 15 * MINUTE_IN_SECONDS );
 }
-//Set post limit
+// Set post limit.
 if ( false === defined( 'WPMSP_POST_LIMIT' ) ) {
 	define( 'WPMSP_POST_LIMIT', 20 );
 }
-//Hook into WordPress
+
+// Hook into WordPress.
 add_action( 'init', 'nv_wpmsp_init', 0 );
-//Plugin Actions
+
+// Plugin Actions.
 add_filter( 'plugin_action_links_' . $nv_wpms_base_name, 'nv_wpmsp_plugin_activation_link', 10, 1 );
 add_filter( 'plugin_row_meta', 'nv_wpmsp_plugin_row_meta', 10, 2 );
 
@@ -35,7 +40,6 @@ add_filter( 'plugin_row_meta', 'nv_wpmsp_plugin_row_meta', 10, 2 );
  * Check timestamp from transient and published all missed posts
  */
 function nv_wpmsp_init() {
-
 	$last_scheduled_missed_time = get_transient( 'wp_scheduled_missed_time' );
 
 	$time = current_time( 'timestamp', 0 );
@@ -48,11 +52,13 @@ function nv_wpmsp_init() {
 
 	global $wpdb;
 
-	$sql_query = "SELECT ID FROM {$wpdb->posts} WHERE ( ( post_date > 0 && post_date <= %s ) ) AND post_status = 'future' LIMIT 0,%d";
-
-	$sql = $wpdb->prepare( $sql_query, current_time( 'mysql', 0 ), WPMSP_POST_LIMIT );
-
-	$scheduled_post_ids = $wpdb->get_col( $sql );
+	$scheduled_post_ids = $wpdb->get_col(
+		$wpdb->prepare(
+			"SELECT ID FROM {$wpdb->posts} WHERE ( ( post_date > 0 && post_date <= %s ) ) AND post_status = 'future' LIMIT 0,%d",
+			current_time( 'mysql', 0 ),
+			WPMSP_POST_LIMIT
+		)
+	);
 
 	if ( ! count( $scheduled_post_ids ) ) {
 		return;
@@ -67,11 +73,10 @@ function nv_wpmsp_init() {
 	}
 }
 
-
 /**
  * Add plugin activation link
  *
- * @param $links
+ * @param array $links Plugin links.
  *
  * @return array
  */
@@ -84,22 +89,21 @@ function nv_wpmsp_plugin_activation_link( $links ) {
 /**
  * Add link in plugin row meta
  *
- * @param $links
+ * @param array  $links Plugin links.
+ * @param string $file Plugin file path.
  *
  * @return array
  */
-
 function nv_wpmsp_plugin_row_meta( $links, $file ) {
 	if ( false === is_admin() ) {
 		return;
 	}
 
-
 	if ( false === current_user_can( 'administrator' ) ) {
 		return;
 	}
 
-	if ( $file == plugin_basename( __FILE__ ) ) {
+	if ( plugin_basename( __FILE__ ) === $file ) {
 		$links[] = '<a href="https://newvariable.com/contact/">' . esc_html__( 'Contact', 'nv-wpmsp' ) . '</a>';
 	}
 
